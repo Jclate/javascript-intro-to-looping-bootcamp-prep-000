face26eed4a958e597b554b0c5b18149138fffbb
Jake Gonchar <jake098877@gmail.com>
2020-04-05 20:07:31 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,36 @@
+var array = []
+function forLoop(array) {
+for (let i = 0; i < 25; i++) {
+  if (i === 0) {
+    array[i] = `I am ${i} strange loop.`
+  }
+  else {
+    array[i] = `I am ${i} strange loops.`
+  }
+}
+/*  console.log(array[i])
+  return (array[i])
+}
+}
+/*
+var number = 100
+function whileLoop(number) {
+  number > -1
+  number--;
+  }
+  while (whileLoop()) {
+  console.log(number)
+  }
+  return "done"
+
+
+let num = 0
+function doWhileLoop() {
+    num = num + 1;
+    return num;
+
+}
+do {
+  console.log("I run once regardless.")
+}
+  while(doWhileLoop() < 10)
