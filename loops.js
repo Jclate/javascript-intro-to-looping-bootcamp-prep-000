c8c42f9f0bdaafa6d688cbab78c3ce64241c1dbc
Jake Gonchar <jake098877@gmail.com>
2020-04-05 21:20:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function whileLoop(n) {
   }
   return "done"
 }
-/*
+
 let num = 0
 function doWhileLoop(num) {
     num = num + 1;
@@ -30,4 +30,4 @@ function doWhileLoop(num) {
 do {
   console.log("I run once regardless.")
 }
-  while(doWhileLoop() < 10)*/
+  while(doWhileLoop() < 10)
