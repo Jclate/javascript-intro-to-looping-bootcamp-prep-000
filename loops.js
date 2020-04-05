4d7a22bad1399a8494f5909f8200b609ccf3657f
Jake Gonchar <jake098877@gmail.com>
2020-04-05 21:10:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,12 +2,12 @@ const array = []
 function forLoop(array) {
 for (let i = 0; i < 25; i++) {
   if (i === 0) {
-    array[i].push = "I am 1 strange loop."
+    array.push = "I am 1 strange loop."
 
 }
   else
 
-    array[i].push = `I am ${i} strange loops.`
+    array.push = `I am ${i} strange loops.`
 
   }
 return array
