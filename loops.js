9330ec1d1c1754ad04023fe2843326fbccc9caa4
Jake Gonchar <jake098877@gmail.com>
2020-04-05 21:01:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 const array = []
 function forLoop(array) {
 for (let i = 0; i < 25; i++) {
-  if (i === 0) {
-    array.push = `I am ${i} strange loop.`
+  if (i === 1) {
+    array.push = "I am 1 strange loop."
 
 }
   else
